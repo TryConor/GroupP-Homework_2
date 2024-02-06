3a80657f544667a51a58fdbe6fa05fdd8f2e9698
Eli Wuerth <emwcd7@umsystem.edu>
2024-02-06 16:33:58 -0600

    testing a push and commit

--- Src/hw2/StudentManager.java
+++ Src/hw2/StudentManager.java
@@ -1,10 +1,7 @@
 package hw2;
 
 public class StudentManager {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	private Student[] students;
+    private int studentCount;
 
 }
