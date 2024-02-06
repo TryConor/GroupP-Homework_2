aede51194ea55ba3301206ac4308d2a576524dc6
Eli Wuerth <emwcd7@umsystem.edu>
2024-02-06 16:39:57 -0600

    added to student to see if it works

--- Src/hw2/StudentManager.java
+++ Src/hw2/StudentManager.java
@@ -1,7 +1,12 @@
 package hw2;
 
+
 public class StudentManager {
 	private Student[] students;
     private int studentCount;
-
+    
+    public StudentManager() {
+        students = new Student[100];
+        studentCount = 0;
+    }
 }
