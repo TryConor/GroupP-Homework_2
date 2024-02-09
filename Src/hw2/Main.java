b96fa4ea7ed9bcaf40be50f480d50ca07155593a
kriswizzle03 <appcr@host.docker.internal>
2024-02-09 17:43:42 -0600

    Instantiated StudentManager, called readFromFile() method specifying studentData.txt is located in the files folder, and called displayStudents() method.

--- Src/hw2/Main.java
+++ Src/hw2/Main.java
@@ -1,10 +1,21 @@
 package hw2;
 
+import hw2.StudentManager;
+import hw2.Student;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Working?");
-		System.out.println("Test");
+		// Instantiate StudentManager, perform operations based on the requirements
+		StudentManager studentManager = new StudentManager();
+		
+		// Read student data from a file and initialize Student objects
+		boolean fileReadOk = studentManager.readFromFile("files/studentData.txt");
+				
+			// If reading from file successful, it displays all the students' data
+			if (studentManager.readFromFile("files/studentData.txt")) {
+				studentManager.displayStudents();
+			}
 
 	}
 
