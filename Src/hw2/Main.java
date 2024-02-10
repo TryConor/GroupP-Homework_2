47e517d87ebbf2efb01cd3ec7d338ae59a9df91b
kriswizzle03 <appcr@host.docker.internal>
2024-02-09 18:11:13 -0600

    Removed redundant readFromFile() call and called searchStudentById() method and updateStudentGradeById() method. After all the updates, called displayStudents() to reflect changes made.

--- Src/hw2/Main.java
+++ Src/hw2/Main.java
@@ -8,15 +8,21 @@ public class Main {
 	public static void main(String[] args) {
 		// Instantiate StudentManager, perform operations based on the requirements
 		StudentManager studentManager = new StudentManager();
-		
-		// Read student data from a file and initialize Student objects
-		boolean fileReadOk = studentManager.readFromFile("files/studentData.txt");
 				
-			// If reading from file successful, it displays all the students' data
-			if (studentManager.readFromFile("files/studentData.txt")) {
-				studentManager.displayStudents();
-			}
-
-	}
+		// If reading from file successful, display all the students' data
+		if (studentManager.readFromFile("files/studentData.txt")) {
+			studentManager.displayStudents();
+		}
+		
+		// Search for student by id and display student found
+        boolean studentFound = studentManager.searchStudentById(101);
+	    
+        // Update the grade of a student by ID
+	    boolean studentGradeUpdateStatus = studentManager.updateStudentGradeById(102, 95);
+	    
+	    //Display all students after the update
+	    studentManager.displayStudents();
+        }
+		
 
 }
